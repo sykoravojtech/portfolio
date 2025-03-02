bc7a7dca96a1791e58111c374bf4e0beeb296258
sykoravojtech <63305039+sykoravojtech@users.noreply.github.com>
2025-03-02 14:41:48 +0100

    update config

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
