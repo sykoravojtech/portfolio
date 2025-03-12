327f754a1c9935243aae7c4220e4fe38490f4f1a
Vojtěch Sýkora <vojtasykor@gmail.com>
2025-03-12 19:02:37 +0100

    projects description

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -71,7 +71,7 @@ const Projects = ({
         {ShowTitle && <Title>Projects</Title>}
         {IntroText && (
           <Desc>
-            I have worked on a wide range of projects. Here are some of my projects.
+            I have worked on a wide range of projects. Here are some of my projects. Click on a project to show more details.
           </Desc>
         )}
         {projectFilters && (
